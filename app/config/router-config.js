a319de7ba2deff31a47a8c0728fb21373dfb9e20
william-skelton <william.f.skelton@outlook.com>
2017-04-28 12:46:12 -0700

    added a bunch of stuff

---
+++ app/config/router-config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
+
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('' , '/join#signup');
+  $urlRouterProvider.when('/' , '/join#signup');
+  $urlRouterProvider.when('/signup' , '/join#signup');
+  $urlRouterProvider.when('/login' , '/join#login');
+
+  let states = [];
+
+  states.forEach( state => {
+    $stateProvider.state(state);
+  });
+};
